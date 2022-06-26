071cb2b46f14b54161afbbebda9efd87d6f1bfca
DEVPOB <86759611+DEVPOB@users.noreply.github.com>
2022-06-26 17:28:51 +0700

    Nutrition & Contact Page

---
+++ Js/Contact.js
@@ -0,0 +1,7 @@
+const bar = document.querySelector('.bar');
+const header = document.querySelector('header');
+bar.onclick = () => {
+    const menu = document.querySelector('nav');
+    menu.classList.toggle('show');
+}
+
---
+++ Js/Nutrition.js
@@ -0,0 +1,15 @@
+const bar = document.querySelector('.bar');
+const header = document.querySelector('header');
+bar.onclick = () => {
+    const menu = document.querySelector('nav');
+    menu.classList.toggle('show');
+}
+function NavbarChangePosition() {
+    if (window.scrollY > 30) {
+        header.style.position = "fixed";
+    } else {
+        header.style.position = "relative";
+    }
+
+}
+window.addEventListener('scroll', NavbarChangePosition);
