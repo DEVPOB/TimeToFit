efd0f86617ef804b4d1d2c79bb2cc9a53ddff9b3
BOB <86759611+DEVPOB@users.noreply.github.com>
2023-10-16 02:06:15 +0700

    Update Calculator

--- Js/Calculator.js
+++ Js/Calculator.js
@@ -10,8 +10,35 @@ function NavbarChangePosition() {
     } else {
         header.style.position = "relative";
     }
-
+    
 }
+let select = document.getElementById('TDEE_ID');
+let BMI = document.getElementById("BMI");
+let BMR = document.getElementById("BMR");
+let TDEE = document.getElementById("TDEE");
+let BODY_FAT = document.getElementById("BODY-FAT");
+let result = "";
+
+let Caloric_Reduction = document.getElementById('Caloric-Reduction');
+let Caloric_Recommend = document.getElementById('Caloric-Recommend');
+
+const MaleBtn = document.querySelector('.GenderMale');
+const FeMaleBtn = document.querySelector('.GenderFeMale');
+MaleBtn.addEventListener('click', () => {
+    result = "male"
+    MaleBtn.classList.add('activeMale'); 
+    FeMaleBtn.classList.remove('activeFeMale');
+});
+FeMaleBtn.addEventListener('click', () => {
+    result = "female"
+    FeMaleBtn.classList.add('activeFeMale'); 
+    MaleBtn.classList.remove('activeMale');
+
+});
+
+
+
+
 
 
 function Calculator() {
@@ -19,28 +46,31 @@ function Calculator() {
     let HeightInput = document.getElementById("Height").value;
     let WeightInput = document.getElementById("Weight").value;
     let WaistInput = document.getElementById("Waist").value;
-    let WristInput = document.getElementById("Wrist").value;
     let HipInput = document.getElementById("Hip").value;
-    let UpperArmInput = document.getElementById("Upper-Arm").value;
+    let NeckInput = document.getElementById("Neck").value;
 
-
-    let result = "";
-    try {
-        let selected = document.querySelector("input[name='gender']:checked").value;
-        result = selected;
-    } catch (error) {
+    const inputIds = ["Age", "Height", "Weight", "Waist", "Hip", "Neck"];
+    for (let i = 0; i < inputIds.length; i++) {
+        const inputValue = document.getElementById(inputIds[i]).value;
+        console.log(inputValue);
+        if (inputValue === "") {
+            alert("กรุณากรอกข้อมูลให้ครบ!");
+            return;
+        }else if(inputValue <= 0){
+            alert("ไม่สามารถกรอกข้อมูลที่น้อยกว่าหรือเท่ากับ 0 ได้")
+            return;
+        }
+    }
+    if(result === ""){
+        alert("กรุณาเลือกเพศของคุณ!");
         return;
     }
-    let select = document.getElementById('TDEE_ID');
-    let BMI = document.getElementById("BMI");
-    let BMR = document.getElementById("BMR");
-    let TDEE = document.getElementById("TDEE");
-    let BODY_FAT = document.getElementById("BODY-FAT");
-    let BMI_Calculated = Math.floor(WeightInput / (HeightInput / 100 * HeightInput / 100))
+    
+    let BMI_Calculated = Math.floor(WeightInput / (HeightInput / 100 * HeightInput / 100));
     BMI.innerHTML = BMI_Calculated;
     if (result == "male") {
         // BMR Calculator
-        let BodyMassMale = Math.floor(66 + (13.7 * WeightInput) + (5 * HeightInput) - (6.8 * AgeInput))
+        let BodyMassMale = Math.floor(66 + (13.7 * WeightInput) + (5 * HeightInput) - (6.8 * AgeInput));
         BMR.innerHTML = BodyMassMale;
         // TDEE
         let value = select.options[select.selectedIndex].value;
@@ -64,12 +94,15 @@ function Calculator() {
         }
 
         // BODY FAT Calculator
-        let Pounds = WeightInput * 2.20;
-        let FactorOne = (Pounds * 1.082) + 94.42;
-        let FactorTwo = (WaistInput * 4.15);
-        let LBM = FactorOne - FactorTwo;
-        let BFM = Pounds - LBM;
-        BODY_FAT.innerHTML = Math.floor((BFM * 100) / Pounds) + "%";
+        WaistInput = WaistInput * 2.54;
+        HipInput = HipInput * 2.54;
+        NeckInput = NeckInput * 2.54;
+
+        const waistNeck = WaistInput - NeckInput;
+
+
+        const bfp = 495 / (1.0324 - 0.19077 * Math.log10(waistNeck) + 0.15456 * Math.log10(HeightInput)) - 450;
+        BODY_FAT.innerHTML = Math.floor(bfp) + "%";
     }
     else if (result == "female") {
         let BodyMassFemale = Math.floor(665 + (9.6 * WeightInput) + (1.8 * HeightInput) - (4.7 * AgeInput))
@@ -94,21 +127,59 @@ function Calculator() {
                 break;
         }
         // BodyFat
-        let FactorOne = (WeightInput * 0.732) + 8.987;
-        console.log(FactorOne);
-        let FactorTwo = (WristInput / 3.140);
-        let FactorThree = (WaistInput * 0.157);
-        let FactorFour = (HipInput * 0.249);
-        let FactorFive = (UpperArmInput * 0.434);
-        let LBM = FactorOne + FactorTwo - FactorThree - FactorFour + FactorFive;
-        let AmountBodyFat = WeightInput - LBM;
-        console.log(AmountBodyFat);
-        BODY_FAT.innerHTML = Math.floor(WeightInput * AmountBodyFat / 100) + "%";
+        WaistInput = WaistInput * 2.54;
+        HipInput = HipInput * 2.54;
+        NeckInput = NeckInput * 2.54;
+        
+        const waistHipNeck = WaistInput + HipInput - NeckInput;
+
+        const bfp = 495 / (1.29579 - 0.35004 * Math.log10(waistHipNeck) + 0.22100 * Math.log10(HeightInput)) - 450;
+        console.log(bfp);
+        BODY_FAT.innerHTML = Math.floor(bfp) + "%";
     }
     
-    if (AgeInput == "" || HeightInput == "" || WeightInput == "" || WaistInput == "") {
+    const Result = document.getElementById("Neck");
+    Result.scrollIntoView({
+        behavior: "smooth"
+    })
+
+}
+
+function DietPlanCal() {
+    let TDEEDiet = parseInt(TDEE.textContent);
+    let Weight_Goal = document.getElementById("Weight_Goal").value;
+    let Day_Goal = document.getElementById("Day_Goal").value;
+    let WeightInput = document.getElementById("Weight").value;
+
+    if (TDEEDiet <= 1) {
+        const CalculatorSection = document.getElementById("Calculate");
+        CalculatorSection.scrollIntoView({
+            behavior: "smooth"
+        })
+        alert("กรุณากรอกข้อมูลข้างบนให้ครบ")
         return;
     }
+    
+    const inputIds = ["Weight_Goal", "Day_Goal",];
+    for (let i = 0; i < inputIds.length; i++) {
+        const inputValue = document.getElementById(inputIds[i]).value;
+        console.log(inputValue);
+        if (inputValue === "") {
+            alert("กรุณากรอกข้อมูลให้ครบ!");
+            return;
+        }else if(inputValue <= 0){
+            alert("ไม่สามารถกรอกข้อมูลที่น้อยกว่าหรือเท่ากับ 0 ได้")
+            return;
+        }
+    }
+
+
+    let DailyCalorie_Raw = (WeightInput - Weight_Goal) / Day_Goal;
+    let DailyCalorie_Reduce = DailyCalorie_Raw * 7700;
+    let Daily_Recommend_Calorie = TDEEDiet - DailyCalorie_Reduce;
+
+    Caloric_Reduction.innerHTML = Math.floor(DailyCalorie_Reduce);
+    Caloric_Recommend.innerHTML = Math.floor(Daily_Recommend_Calorie);
 
 }
 
